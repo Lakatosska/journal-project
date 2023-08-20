ac4d421765b3226f96aafdaa4807fcacbd5fffe3
Elena Nikiforova <vizualnoe@gmail.com>
2023-08-20 23:09:42 +0200

    add svg and file loaders, ui kit - button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import "app/styles/index.scss";
 import { useTheme } from "app/providers/ThemeProvider";
-import { classNames } from "../shared/lib/classNames";
+import { classNames } from "../shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 
