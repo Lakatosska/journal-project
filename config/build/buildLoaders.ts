6ce983386218abd3541dd7a7fd8e4d0da0dd22fb
Elena Nikiforova <vizualnoe@gmail.com>
2023-08-21 19:39:37 +0200

    add babel, extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -6,7 +6,26 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
   const svgLoader = {
     test: /\.svg$/,
     use: ['@svgr/webpack']
-}
+  }
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env'],
+        "plugins": [
+          [
+            "i18next-extract",
+            {
+                locales: ['ru', 'en'],
+                keyAsDefaultValue: true
+            }
+          ],
+        ]
+      }
+    }
+  }
   const cssLoader = { 
       test: /\.s[ac]ss$/i,
       use: [
@@ -41,9 +60,10 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     }
 
     return [
-        typescriptLoader,
-        cssLoader,
-        svgLoader,
-        fileLoader
+      fileLoader,
+      svgLoader,
+      babelLoader,
+      typescriptLoader,
+      cssLoader,
     ]
 }
